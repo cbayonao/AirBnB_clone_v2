7f691654dd1b702fad9c7b38e5ecbffc4055fbce
cristian0497 <cristian04.97@hotmail.com>
2020-03-30 21:01:16 +0000

    [ADD] Create a User and DB

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- create a DB
---name hbbn
-CREATE IF NOT EXISTS DATABASE hbhb_dev_db;
-CREATE IF NOT EXISTS USER 'hbhb_dev'@'localhost';
+-- name hbbn
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_scheme.* TO 'hbnb_dev'@'localhost';
