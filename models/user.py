263efe7e39bc2837db47dcb9112af11e695d88dd
cbayonao <drcamilob@live.com>
2020-04-02 14:14:28 -0500

    [Update] place and user model for relation with place

--- models/user.py
+++ models/user.py
@@ -23,3 +23,6 @@ class User(BaseModel, Base):
     places = relationship("Place",
                           backref="user",
                           cascade="all, delete, delete-orphan")
+    reviews = relationship("Review",
+                           backref="user",
+                           cascade="all, delete, delete-orphan")
