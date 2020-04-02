4d98d59dc6c9d7f4b4c1a536e7705e2be994ddbf
Camilo Bayona <drcamilob@live.com>
2020-04-01 23:19:52 -0500

    Revert "[Feature]"

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
 class Place(BaseModel):
--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
 class Review(BaseModel):
