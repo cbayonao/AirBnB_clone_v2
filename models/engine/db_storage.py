e976d6e955f07b0b4990ce50d0d13f748ec9c298
cbayonao <drcamilob@live.com>
2020-04-01 20:36:10 -0500

    [Feature] Update reload, update all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-DBStorage - States and Cities
-"""
+"""DBStorage - States and Cities"""
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -12,9 +10,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-"""
-1. New engine DBStorage: (models/engine/db_storage.py)
-"""
 
 
 class DBStorage:
@@ -22,15 +17,13 @@ class DBStorage:
     2. Private class attributes:
     __engine: set to None
     __session: set to None
+    3. Public instance methods:
+    __init__(self):
     """
     __engine = None
     __session = None
     models = {User, State, City, Amenity, Place, Review}
 
-    """
-    3. Public instance methods:
-    __init__(self):
-    """
     def __init__(self):
         """
         4. all of the following values must be retrieved via
@@ -41,18 +34,16 @@ class DBStorage:
         MySQL database: HBNB_MYSQL_DB
         don’t forget the option pool_pre_ping=True when you call create_engine
         drop all tables if the environment variable HBNB_ENV is equal to test
-        """
-        user = getenv("HBNB_MYSQL_USER")
-        password = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
-        db = getenv("HBNB_MYSQL_DB")
-        """
         5. create the engine (self.__engine)
         the engine must be linked to the MySQL database and user created
         before (hbnb_dev and hbnb_dev_db):
         dialect: mysql
         driver: mysqldb
         """
+        user = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(user, password, host, db),
                                       pool_pre_ping=True)
@@ -70,15 +61,19 @@ class DBStorage:
         key = <class-name>.<object-id>
         value = object
         """
-        sql_dict = {}
-        models = self.models
         if cls:
-            models = {cls}
-        for model in models:
-            objects = self.__session.query(model).all()
-            for obj in objects:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                sql_dict[key] = obj
+            obj = self.__session.query(self.classes()[cls])
+        else:
+            obj = self.__session.query(State).all()
+            obj = obj + self.__session.query(City).all()
+            obj = obj + self.__session.query(User).all()
+            obj = obj + self.__session.query(Place).all()
+            obj = obj + self.__session.query(Amenity).all()
+            obj = obj + self.__session.query(Review).all()
+        sql_dict = {}
+        for o in obj:
+            key = '{}.{}'.format(type(obj).__name__, o.id)
+            sql_dict[key] = o
         return sql_dict
 
     def new(self, obj):
@@ -101,7 +96,7 @@ class DBStorage:
         9. delete(self, obj=None): delete from the current
         database session obj if not None.
         """
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
@@ -117,6 +112,6 @@ class DBStorage:
         and scoped_session to make sure your Session is thread-safe
         """
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(Session)
+        self.__session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(self.__session)
         self.__session = Session()
