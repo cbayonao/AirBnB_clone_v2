435e4b6059dafbfda8b1d436aa915e763f627952
cbayonao <drcamilob@live.com>
2020-04-11 16:57:00 -0500

    [Change] dont run do deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from fabric.api import *
 import os.path
-import re
 from datetime import datetime
 env.hosts = ['35.196.71.231', '3.89.186.246']
 env.user = 'ubuntu'
@@ -17,8 +16,10 @@ def do_pack():
         todayDate = now.strftime("%Y%m%d%H%M%S")
         cPath = "versions/web_static_" + todayDate
         local("tar -cvzf {}.tgz web_static".format(cPath))
+        print("Archivo empaquetado")
         return cPath
     except:
+        print("No se pudo empaquetar")
         return None
 
 
@@ -27,21 +28,32 @@ def do_deploy(archive_path):
     Deploy archive!
     """
     try:
-        if not os.path.exists(archive_path):
-            return False
-        put(archive_path, "/tmp/")
         fileComp = archive_path.split("/")[1].split(".")[0]
         path = "/data/web_static/releases/{}".format(fileComp)
 
+        put(archive_path, "/tmp/")
+        print("Archivo puesto")
+
         run("mkdir -p {}".format(path))
+        print("Carpeta path creada")
+
         run("tar -xvzf /tmp/{}.tgz -C {}".format(fileComp, path))
-        run("sudo rm /tmp/{}.tgz".format(fileComp))
-        run("sudo rm /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/{}\
-        /data/web_static/current".format(fileComp))
+        print("Descomprimido")
+
         run("sudo mv /data/web_static/releases/{}/web_static/* \
         /data/web_static/releases/{}/".format(fileComp, fileComp))
+        print("Archivos movidos")
+
+        run("sudo rm -rf /tmp/{}.tgz".format(fileComp))
+        print("Archivo tgz eliminado")
+        run("sudo rm -rf /data/web_static/current")
+
+        run("sudo ln -sf /data/web_static/releases/{}\
+        /data/web_static/current".format(fileComp))
+        print("Simbolik link ready")
+
         run("rm -rf /data/web_static/releases/{}/web_static".format(fileComp))
+        print("Eliminando web_static remotamente")
         return True
     except:
         return False
@@ -51,8 +63,10 @@ def deploy():
     """
     Full deployment
     """
-    f_path = do_pack()
-    if f_path is None:
+    archive_path = do_pack()
+
+    if archive_path is None:
+        print("No hay archive_path en Deploy")
         return False
-    ready = do_deploy(f_path)
-    return ready
+
+    return do_deploy(archive_path)
