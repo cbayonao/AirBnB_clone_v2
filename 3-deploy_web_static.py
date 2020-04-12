91456f1062dc9679396093efd1d48f384fa139ae
cbayonao <drcamilob@live.com>
2020-04-11 21:47:16 -0500

    [Fix] Julian3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     try:
         now = datetime.now()
         todayDate = now.strftime("%Y%m%d%H%M%S")
-        cPath = "versions/web_static_" + todayDate
+        cPath = "versions/web_static_{:s}.tgz".format(todayDate)
         local("mkdir -p versions")
         local("tar -cvzf {} web_static".format(cPath))
         return cPath
